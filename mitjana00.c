12fcc05d48d3d9ec31511dca4fdf4cfe1a40692e
xavara22daw <xavara22daw@proton.me>
2023-02-01 18:45:38 +0100

    Conflicte 1 entre main i br0

--- mitjana00.c
+++ mitjana00.c
@@ -22,6 +22,7 @@ int main() {
 		scanf(" %c",&consulta); 
 	}
 	mitjana=suma/numero_repeticions;
-	printf("La mitjana resultant és: %.2f\n",mitjana);	
+	printf("La mitjana resultant és: %.2f\n",mitjana);
+	printf("La mitjana ha estat trobada amb èxit\n");
 	return 0;
 }
